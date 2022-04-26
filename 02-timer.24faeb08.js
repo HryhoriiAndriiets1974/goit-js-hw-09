2c7e3cda03af79e4131b8cf1e2ceb7bf12ddd0f1
HryhoriiAndriiets1974 <93792925+HryhoriiAndriiets1974@users.noreply.github.com>
2022-04-26 11:29:11 +0000

    Deploying to gh-pages from @ HryhoriiAndriiets1974/goit-js-hw-09@83a72abf72298a0d7d1d9ad3a2a336e34f7a3772 🚀

--- 02-timer.24faeb08.js
+++ 02-timer.24faeb08.js
@@ -17,6 +17,6 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./types/options":"lJVm","./l10n/default":"SFEe","./utils":"U4Vg","./utils/dom":"UwHZ","./utils/dates":"viQd","./utils/formatting":"KtbI","./utils/polyfills":"pFfd"}],"c0Wa":[function(require,module,exports) {
 
 },{}],"d2uN":[function(require,module,exports) {
-"use strict";var r=e(require("flatpickr"));function e(r){return r&&r.__esModule?r:{default:r}}require("flatpickr/dist/flatpickr.min.css");
+"use strict";var e=o(require("flatpickr"));function o(e){return e&&e.__esModule?e:{default:e}}require("flatpickr/dist/flatpickr.min.css");const t={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};function l(e){return{days:Math.floor(e/864e5),hours:Math.floor(e%864e5/36e5),minutes:Math.floor(e%864e5%36e5/6e4),seconds:Math.floor(e%864e5%36e5%6e4/1e3)}}console.log(l(2e3)),console.log(l(14e4)),console.log(l(2414e4));
 },{"flatpickr":"BKNP","flatpickr/dist/flatpickr.min.css":"c0Wa"}]},{},["d2uN"], null)
-//# sourceMappingURL=/goit-js-hw-09/02-timer.1c96744c.js.map
+//# sourceMappingURL=/goit-js-hw-09/02-timer.24faeb08.js.map
