f8bf9d2448722bc43d29166575ce392d7f3fb315
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-04-27 00:05:06 +0300

    hw-09-task-02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,22 @@
 import flatpickr from "flatpickr";
 // Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
+/** замість window.alert() */
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+let dateUnix = null;
+
+const { input, startBtn, days_, hours_, minutes_, seconds_ } = {
+  input: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  days_: document.querySelector('[data-days]'),
+  hours_: document.querySelector('[data-hours]'),
+  minutes_: document.querySelector('[data-minutes]'),
+  seconds_: document.querySelector('[data-seconds]'),
+};
+
+startBtn.disabled = true;
+startBtn.addEventListener('click', onStartClick);
 
 const options = {
   enableTime: true,
@@ -10,9 +26,55 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    if (selectedDates[0] < options.defaultDate) {
+      return Notify.failure('Please choose a date in the future');
+    }
+    startBtn.disabled = false;
   },
 };
 
+/** згідно документації для запуску Flatpickr */
+const flatP = flatpickr('#datetime-picker', options);
+
+function onStartClick() {
+  dateUnix = setInterval(setTime, 1000);
+  startBtn.disabled = true;
+  input.disabled = true;
+}
+
+function setTime() {
+  const deltaTime = flatP.selectedDates[0].getTime() - Date.now();
+  console.log(deltaTime);
+    if (deltaTime < 700) {
+      finishTimer();
+      Notify.success('Timer off', { timeout: 7000 });
+}
+  const { days, hours, minutes, seconds } = convertMs(deltaTime);
+  if (deltaTime > 0) {
+    days_.textContent = days;
+    hours_.textContent = hours;
+    minutes_.textContent = minutes;
+    seconds_.textContent = seconds;
+  }
+}
+
+function finishTimer () {
+  clearInterval(dateUnix);
+  input.disabled = false;
+  days_.textContent = '00';
+  hours_.textContent = '00';
+  minutes_.textContent = '00';
+  seconds_.textContent = '00';
+};
+
+/** перед рендерингом інтефрейсу форматує значення часу */
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+/** підрахунок значень одиниць часу секунд, хвилин, годин, днів.
+ *  де ms - різниця між кінцевою і поточною датою в мілісекундах.
+*/
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
